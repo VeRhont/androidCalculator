f0cabf5dff2ba4eed4b1613f73ff7ca136299bfd
Semyon Ivanov <semeni07@mail.ru>
2024-07-27 19:54:30 +0300

    Working calculator

---
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -0,0 +1,61 @@
+package com.example.calculator
+
+import android.util.Log
+import java.util.LinkedList
+import java.util.Queue
+
+
+class Calculator {
+
+    private var numbers: Queue<Double> = LinkedList()
+    private var operations: Queue<String> = LinkedList()
+
+    fun addOperation(operation: String) {
+        operations.add(operation)
+    }
+
+    fun addElement(element: Double) {
+        numbers.add(element)
+    }
+
+    fun getResult(): Double {
+
+        operations.forEach {
+            Log.d("operation", it)
+        }
+
+        numbers.forEach {
+            Log.d("number", it.toString())
+        }
+
+        var result = numbers.remove()
+
+        while (operations.isNotEmpty()) {
+            val operation = operations.remove()
+            val number1 = numbers.remove()
+
+            result = when (operation) {
+                "+" -> {
+                    result + number1
+                }
+                "-" -> {
+                    result - number1
+                }
+                "*" -> {
+                    result * number1
+                }
+                "/" -> {
+                    result / number1
+                }
+                else -> {
+                    0.0
+                }
+            }
+        }
+
+        numbers.clear()
+        operations.clear()
+
+        return result
+    }
+}
--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,20 +1,94 @@
 package com.example.calculator
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var currentNumber: String = "0"
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val calculator = Calculator()
+
+        val buttonNumbers = listOf(
+            binding.btn0,
+            binding.btn1,
+            binding.btn2,
+            binding.btn3,
+            binding.btn4,
+            binding.btn5,
+            binding.btn6,
+            binding.btn7,
+            binding.btn8,
+            binding.btn9
+        )
+
+        val buttonOperations = mapOf(
+            binding.btnAdd to "+",
+            binding.btnDiv to "/",
+            binding.btnMul to "*",
+            binding.btnSub to "-"
+        )
+
+        buttonNumbers.forEachIndexed { index, button ->
+            button.setOnClickListener {
+                if (currentNumber == "0") {
+                    currentNumber = index.toString()
+                } else {
+                    currentNumber += index.toString()
+                }
+                binding.tvResult.text = currentNumber
+            }
+        }
+
+        buttonOperations.forEach { operation ->
+            operation.key.setOnClickListener {
+                calculator.addElement(currentNumber.toDouble())
+                calculator.addOperation(operation.value)
+                currentNumber = "0"
+                binding.tvResult.text = currentNumber
+            }
+        }
+
+        binding.btnPoint.setOnClickListener {
+            if (currentNumber.contains('.')) {
+                Toast.makeText(this, "Incorrect number", Toast.LENGTH_SHORT).show()
+            } else {
+                currentNumber += "."
+                binding.tvResult.text = currentNumber
+            }
+        }
+
+        binding.btnClear.setOnClickListener {
+            currentNumber = "0"
+            binding.tvResult.text = currentNumber
+        }
+
+        binding.btnResult.setOnClickListener {
+            calculator.addElement(currentNumber.toDouble())
+            val result = calculator.getResult()
+            currentNumber = result.toString()
+            binding.tvResult.text = currentNumber
         }
     }
 }
